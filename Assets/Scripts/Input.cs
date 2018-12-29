7a28837e4781bdd919d1fa5e4b47290679fa64f1
SpectatorQL <hlcombine@gmail.com>
2018-12-29 20:45:50 +0100

    Implemented basic movement Put the map image in the background

---
+++ Assets/Scripts/Input.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace Speedmarket
+{
+    public class PlayerController
+    {
+        public PlayerInput OldInput;
+        public PlayerInput NewInput;
+    }
+    
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct PlayerInput
+    {
+        public float MouseX;
+        public float MouseY;
+        public bool LmbDown;
+        public bool RmbDown;
+
+        public bool W;
+        public bool S;
+        public bool A;
+        public bool D;
+
+        public bool LShift;
+        
+        public static void ProcessKeyboard(MonoBehaviour ent, PlayerController controller)
+        {
+            float speed = 1.0f;
+            float dtSpeed = speed * Time.deltaTime;
+            if(controller.NewInput.LShift)
+            {
+                dtSpeed *= 10.0f;
+            }
+
+            Vector3 pos = ent.transform.position;
+            if(controller.NewInput.W)
+            {
+                pos += ent.transform.up * dtSpeed;
+            }
+            if(controller.NewInput.S)
+            {
+                pos -= ent.transform.up * dtSpeed;
+            }
+            if(controller.NewInput.A)
+            {
+                pos -= ent.transform.right * dtSpeed;
+            }
+            if(controller.NewInput.D)
+            {
+                pos += ent.transform.right * dtSpeed;
+            }
+            ent.transform.position = pos;
+        }
+
+        public static void ProcessMouse(MonoBehaviour ent, PlayerController controller)
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/Movement_Test.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Speedmarket
+{
+    public class Movement_Test : MonoBehaviour
+    {
+        PlayerController _controller = new PlayerController();
+
+        void Update()
+        {
+            _controller.OldInput = _controller.NewInput;
+            _controller.NewInput = new PlayerInput();
+            
+            if(Input.GetKey(KeyCode.W))
+            {
+                _controller.NewInput.W = true;
+            }
+            if(Input.GetKey(KeyCode.S))
+            {
+                _controller.NewInput.S = true;
+            }
+            if(Input.GetKey(KeyCode.A))
+            {
+                _controller.NewInput.A = true;
+            }
+            if(Input.GetKey(KeyCode.D))
+            {
+                _controller.NewInput.D = true;
+            }
+
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                _controller.NewInput.LShift = true;
+            }
+
+            PlayerInput.ProcessKeyboard(this, _controller);
+        }
+    }
+}

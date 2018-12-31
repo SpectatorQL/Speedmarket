c913bd8eb6e857c9964874d8a810baae5d35704d
SpectatorQL <hlcombine@gmail.com>
2018-12-31 13:01:16 +0100

    Basic sprinting

--- Assets/Scripts/Input.cs
+++ Assets/Scripts/Input.cs
@@ -19,43 +19,81 @@ namespace Speedmarket
         public bool LmbDown;
         public bool RmbDown;
 
-        public bool W;
-        public bool S;
-        public bool A;
-        public bool D;
+        public bool WDown;
+        public bool SDown;
+        public bool ADown;
+        public bool DDown;
 
-        public bool LShift;
+        public bool FDown;
+        public bool FUp;
+
+        public bool Debug_LShift;
         
-        public static void ProcessKeyboard(MonoBehaviour ent, PlayerController controller)
+        public static void ProcessKeyboard(PlayerEntity ent, PlayerController controller)
         {
             float speed = 1.0f;
             float dtSpeed = speed * Time.deltaTime;
-            if(controller.NewInput.LShift)
+            if(controller.NewInput.Debug_LShift)
             {
                 dtSpeed *= 10.0f;
             }
 
+            // TODO: Disable sprinting if the character is not moving during the current frame.
+            float time = Time.time;
+            int sprintModifier = 4;
+            float sprintValue = 3.0f;
+            if((time - ent.NextSprint) > 0)
+            {
+                if(controller.NewInput.FUp)
+                {
+                    ent.NextSprint = time + ent.CooldownSecs;
+                }
+                else
+                {
+                    if(controller.NewInput.FDown)
+                    {
+                        if(ent.Sprint > 0)
+                        {
+                            dtSpeed *= sprintValue;
+                            ent.Sprint -= sprintModifier;
+                        }
+                        else
+                        {
+                            ent.NextSprint = time + ent.CooldownSecs;
+                        }
+                    }
+                    else
+                    {
+                        if(ent.Sprint != ent.SprintMax)
+                        {
+                            ent.Sprint += sprintModifier;
+                        }
+                    }
+                }
+            }
+            Debug.Log("Sprint: " + ent.Sprint);
+            
             Vector3 pos = ent.transform.position;
-            if(controller.NewInput.W)
+            if(controller.NewInput.WDown)
             {
                 pos += ent.transform.up * dtSpeed;
             }
-            if(controller.NewInput.S)
+            if(controller.NewInput.SDown)
             {
                 pos -= ent.transform.up * dtSpeed;
             }
-            if(controller.NewInput.A)
+            if(controller.NewInput.ADown)
             {
                 pos -= ent.transform.right * dtSpeed;
             }
-            if(controller.NewInput.D)
+            if(controller.NewInput.DDown)
             {
                 pos += ent.transform.right * dtSpeed;
             }
             ent.transform.position = pos;
         }
 
-        public static void ProcessMouse(MonoBehaviour ent, PlayerController controller)
+        public static void ProcessMouse(PlayerEntity ent, PlayerController controller)
         {
         }
     }
---
+++ Assets/Scripts/PlayerEntity.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Speedmarket
+{
+    public class PlayerEntity : MonoBehaviour
+    {
+        PlayerController _controller = new PlayerController();
+        [HideInInspector] public int Sprint = 1000;
+        [HideInInspector] public int SprintMax = 1000;
+        [HideInInspector] public float CooldownSecs = 2.0f;
+        [HideInInspector] public float NextSprint = 0.0f;
+
+        void Update()
+        {
+            Debug.Log(CooldownSecs);
+
+            _controller.OldInput = _controller.NewInput;
+            _controller.NewInput = new PlayerInput();
+            
+            if(Input.GetKey(KeyCode.W))
+            {
+                _controller.NewInput.WDown = true;
+            }
+            if(Input.GetKey(KeyCode.S))
+            {
+                _controller.NewInput.SDown = true;
+            }
+            if(Input.GetKey(KeyCode.A))
+            {
+                _controller.NewInput.ADown = true;
+            }
+            if(Input.GetKey(KeyCode.D))
+            {
+                _controller.NewInput.DDown = true;
+            }
+
+            if(Input.GetKey(KeyCode.F))
+            {
+                _controller.NewInput.FDown = true;
+            }
+            if(Input.GetKeyUp(KeyCode.F))
+            {
+                _controller.NewInput.FUp = true;
+            }
+
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                _controller.NewInput.Debug_LShift = true;
+            }
+
+            PlayerInput.ProcessKeyboard(this, _controller);
+        }
+    }
+}
